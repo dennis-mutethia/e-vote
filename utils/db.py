3d1b2b761afa51505218c0715530cde1f446b335
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-02-11 11:10:32 +0300

    use neon

--- utils/db.py
+++ utils/db.py
@@ -9,7 +9,6 @@ class Db():
         # Access the environment variables
         self.conn_params = {
             'host': os.getenv('DB_HOST'),
-            'port': os.getenv('DB_PORT'),
             'database': os.getenv('DB_NAME'),
             'user': os.getenv('DB_USER'),
             'password': os.getenv('DB_PASSWORD')
